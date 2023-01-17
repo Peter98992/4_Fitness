645205f530aa65fdee6e3293d281e68b8016db5f
Peter98992 <ppeetteerr05@yahoo.com>
2023-01-18 01:03:02 +0200

    The Remake of the 4Ftiness app UI for first page

--- app/src/main/java/com/example/a4fitness_1/MainActivity.kt
+++ app/src/main/java/com/example/a4fitness_1/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.a4fitness_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //OPENS SETTINGS ACTIVITY
+    val settingsImageView = findViewById<ImageView>(R.id.image_settings)
+
+        settingsImageView.setOnClickListener{
+            val intentForSettings = Intent(this, activity_settings::class.java)
+            startActivity(intentForSettings)
+        }
+
     }
 }
